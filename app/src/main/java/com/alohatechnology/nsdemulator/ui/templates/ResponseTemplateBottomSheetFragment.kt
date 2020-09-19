ddb3c5a1b7bc129306c734f34e3b37e8e125d538
Ismail Shaikh <ismailshaikh31097@gmail.com>
2020-09-19 19:16:27 +0530

    changed bottom sheet fragment layout name

--- app/src/main/java/com/alohatechnology/nsdemulator/ui/templates/ResponseTemplateBottomSheetFragment.kt
+++ app/src/main/java/com/alohatechnology/nsdemulator/ui/templates/ResponseTemplateBottomSheetFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.alohatechnology.nsdemulator.R
-import com.alohatechnology.nsdemulator.databinding.ActivityResponseTemplateBinding
+import com.alohatechnology.nsdemulator.databinding.ResponseTemplateBottomSheetFragmentBinding
 import com.alohatechnology.nsdemulator.util.responseTemplates
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ResponseTemplateBottomSheetFragment : BottomSheetDialogFragment(), ResponseTemplateAdapter.ResponseTemplateClickListener {
 
-    private var binding: ActivityResponseTemplateBinding? = null
+    private var binding: ResponseTemplateBottomSheetFragmentBinding? = null
 
     companion object {
         val CLASS_NAME = ResponseTemplateBottomSheetFragment::class.java.simpleName
@@ -21,7 +21,7 @@ class ResponseTemplateBottomSheetFragment : BottomSheetDialogFragment(), Respons
     var listener: ResponseTemplateAdapter.ResponseTemplateClickListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.activity_response_template, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.response_template_bottom_sheet_fragment, container, false)
         return binding?.root
     }
 
